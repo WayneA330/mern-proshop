e57afc38b72e9da0a16bb4cf021dec70c61c7e88
Wayne Celestin <>
2023-11-02 18:38:45 +0400

    Auth Middleware & Updated endpoints woth middlewares

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,13 +10,18 @@ import {
   getUserByID,
   updateUser,
 } from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(registerUser).get(getUsers);
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.route("/logout").post(logoutUser);
-router.route("/login").post(authUser);
-router.route("/profile").put(updateUserProfile).get(getUserProfile);
-router.route("/:id").delete(deleteUser).get(getUserByID).put(updateUser);
+router.route("/auth").post(authUser);
+router.route("/profile").put(protect, updateUserProfile).get(protect, getUserProfile);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserByID)
+  .put(protect, admin, updateUser);
 
 export default router;
