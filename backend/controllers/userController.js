bc6b5f73d8530f9fbf69b4a806f801fb3f49fb9b
Wayne Celestin <>
2023-11-02 23:38:49 +0400

    logout user & clear cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,7 +46,14 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route    POST /api/users/logout
 // @public   Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("Logout User");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({
+    message: "Logged out successfully",
+  });
 });
 
 // @desc     Get user profile
